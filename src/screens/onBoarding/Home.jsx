354333e1f5902a431e74b1dcafb1ed5c48a84c2e
Vinodratho <102621417+Vinodratho@users.noreply.github.com>
2022-11-14 15:11:43 +0530

    new lead

--- src/screens/onBoarding/Home.jsx
+++ src/screens/onBoarding/Home.jsx
@@ -92,7 +92,7 @@ const HomePage = () => {
               rel="noreferrer noopener"
               className="acceleratingLink"
             >
-              Vinod wants to know how a leading fintech accelerated collections with
+              Hey buddy Saarthi know how a leading fintech accelerated collections with
               85% cost reduction? Download our Success Study.
             </a>
           </h2>
