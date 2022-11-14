a00d37975eaa2393f8ad123c8250a175376dd14d
Vinodratho <102621417+Vinodratho@users.noreply.github.com>
2022-11-14 18:18:47 +0530

    goodevening

--- src/screens/onBoarding/Home.jsx
+++ src/screens/onBoarding/Home.jsx
@@ -92,7 +92,7 @@ const HomePage = () => {
               rel="noreferrer noopener"
               className="acceleratingLink"
             >
-              (Stage)Saarthi know how a leading fintech accelerated collections with
+              (Newstage)Saarthi know how a leading fintech accelerated collections with
               85% cost reduction? Download our Success Study.
             </a>
           </h2>
