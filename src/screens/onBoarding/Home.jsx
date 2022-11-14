fe3690385a649ad9e475bdbdb430ccf644919226
Vinodratho <102621417+Vinodratho@users.noreply.github.com>
2022-11-14 12:40:04 +0530

    change

--- src/screens/onBoarding/Home.jsx
+++ src/screens/onBoarding/Home.jsx
@@ -92,7 +92,7 @@ const HomePage = () => {
               rel="noreferrer noopener"
               className="acceleratingLink"
             >
-              Want to know how a leading fintech accelerated collections with
+              saarthi4334 to know how a leading fintech accelerated collections with
               85% cost reduction? Download our Success Study.
             </a>
           </h2>
