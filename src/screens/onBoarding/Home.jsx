1b60891ab499e34f15ed0b2ef902d3a451153759
Vinodratho <102621417+Vinodratho@users.noreply.github.com>
2022-11-15 11:55:06 +0530

    Tuesday

--- src/screens/onBoarding/Home.jsx
+++ src/screens/onBoarding/Home.jsx
@@ -92,7 +92,7 @@ const HomePage = () => {
               rel="noreferrer noopener"
               className="acceleratingLink"
             >
-              Hey Pravid know how a leading fintech accelerated collections with
+              Hey its Tuesday know how a leading fintech accelerated collections with
               85% cost reduction? Download our Success Study.
             </a>
           </h2>
